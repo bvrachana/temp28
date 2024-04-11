f3a9c7d8659561c83cadac113f5f6328e6531e45
bvracahana <rachanabv2003@gmail.com>
2024-04-11 21:25:48 +0530

    files

--- Basics/Assets/script.js
+++ Basics/Assets/script.js
@@ -1,5 +1,6 @@
  function test(){ //named function
         alert('Hi this is the test function...!')
         document.getElementById('testBtn').innerText = "button is clicked"
+        alert('hello')
     }
     
